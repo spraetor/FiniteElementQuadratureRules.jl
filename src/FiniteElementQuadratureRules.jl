34a1a4b8239a3c3f99ed543998215d3b92e0e08d
Simon Praetorius <simon.praetorius@tu-dresden.de>
2025-01-17 21:19:59 +0100

    Relax version requirements

--- src/FiniteElementQuadratureRules.jl
+++ src/FiniteElementQuadratureRules.jl
@@ -3,8 +3,9 @@ module FiniteElementQuadratureRules
 abstract type AbstractDomain{D} end
 
 include("domain.jl")
-include("generate.jl")
 include("quadraturerule.jl")
+
+include("generate.jl")
 
 export AbstractDomain, Point, Line, Triangle, Quadrilateral, Tetrahedron, Hexahedron, Prism, Pyramid, dimension
 export CompactQuadratureRule, QuadratureRule, generate
