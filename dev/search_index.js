484faf0c5a6079408402b9e2a899ef6fd8233603
Documenter.jl <documenter@juliadocs.github.io>
2025-01-17 20:23:12 +0000

    build based on 34a1a4b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FiniteElementQuadratureRules","category":"page"},{"location":"#FiniteElementQuadratureRules","page":"Home","title":"FiniteElementQuadratureRules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FiniteElementQuadratureRules.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FiniteElementQuadratureRules]","category":"page"}]
+}
