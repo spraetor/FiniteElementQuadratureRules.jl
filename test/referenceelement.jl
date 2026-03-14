4439b8be5a097f9137f7a823f4c05c03970e54e6
Simon Praetorius <simon.praetorius@tu-dresden.de>
2026-03-15 00:46:31 +0100

    Adda few more tests

--- test/referenceelement.jl
+++ test/referenceelement.jl
@@ -3,39 +3,68 @@ using FiniteElementQuadratureRules: checkInside, checkStrictlyInside
 
 @testset "ReferenceElement" begin
 
-  for Domain in Base.uniontypes(FiniteElementQuadratureRules.AllDomains)
-    domain = Domain()
-    ref = ReferenceElement(domain)
+  @testset "Properties of the reference element" begin
+    for Domain in Base.uniontypes(FiniteElementQuadratureRules.AllDomains)
+      domain = Domain()
+      ref = ReferenceElement(domain)
 
-    @test domaintype(ref) == Domain
-    @test dimension(ref) == dimension(domain)
-    @test length(ref.coordinates) == vertices(domain)
-    @test length(ref.facets) == facets(domain)
+      @test domaintype(ref) == Domain
+      @test dimension(ref) == dimension(domain)
+      @test length(ref.coordinates) == vertices(domain)
+      @test length(ref.facets) == facets(domain)
 
-    # Element center (codim 0): inside and, except for Point, strictly inside.
-    x0 = position(ref, 1, 0)
-    @test checkInside(ref, x0)
-    if dimension(domain) > 0
-      @test checkStrictlyInside(ref, x0)
+      # Element center (codim 0): inside and, except for Point, strictly inside.
+      x0 = position(ref, 1, 0)
+      @test checkInside(ref, x0)
+      if dimension(domain) > 0
+        @test checkStrictlyInside(ref, x0)
+      end
+
+      # Test the direct size methods
+      @test size(ref, 0) == 1
+      @test size(ref, dimension(ref)) == vertices(domain)
+      if dimension(ref) > 1
+        @test size(ref, 1) == facets(domain)
+      end
+      if domain == Tetrahedron()
+        @test size(ref,2) == 6
+      elseif domain == Pyramid()
+        @test size(ref,2) == 8
+      elseif domain == Prism()
+        @test size(ref,2) == 9
+      elseif domain == Hexahedron()
+        @test size(ref,2) == 12
+      end
+
+      # Test the sub-size methods
+      @test size(ref, 1,0,0) == 1
+      if dimension(ref) > 1
+        for i in eachindex(ref.facets)
+          @test size(ref, i,1,dimension(ref)) == length(ref.facets[i])
+        end
+      end
+
     end
   end
 
-  # Centers of boundary sub-entities should lie on the boundary:
-  # inside, but not strictly inside.
-  for Domain in Base.uniontypes(FiniteElementQuadratureRules.AllDomains)
-    domain = Domain()
-    ref = ReferenceElement(domain)
-    dim = dimension(domain)
+  @testset "CheckInside" begin
+    # Centers of boundary sub-entities should lie on the boundary:
+    # inside, but not strictly inside.
+    for Domain in Base.uniontypes(FiniteElementQuadratureRules.AllDomains)
+      domain = Domain()
+      ref = ReferenceElement(domain)
+      dim = dimension(domain)
 
-    # check vertices and facets
-    codims = (Int[], [1], [1,2], [1,3])[dim+1]
+      # check vertices and facets
+      codims = (Int[], [1], [1,2], [1,3])[dim+1]
 
-    for c in codims
-      n = c == 1 ? facets(domain) : vertices(domain)
-      for i in 1:n
-        x = position(ref, i, c)
-        @test checkInside(ref, x)
-        @test !checkStrictlyInside(ref, x)
+      for c in codims
+        n = c == 1 ? facets(domain) : vertices(domain)
+        for i in 1:n
+          x = position(ref, i, c)
+          @test checkInside(ref, x)
+          @test !checkStrictlyInside(ref, x)
+        end
       end
     end
   end
